e0b8a4756292c035b40e2ef3be5b1fb0b240ed55
tnn1nja <114170150+tnn1nja@users.noreply.github.com>
2023-12-15 21:27:51 +0000

    Fixed File Order

--- BFDownloader.py
+++ BFDownloader.py
@@ -1,4 +1,18 @@
 import os
+import re
+
+#Order Files
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
 
 #Format Files
 def format(name):
@@ -6,6 +20,7 @@ def format(name):
     lines = []
 
     files = os.listdir(".")
+    files.sort(key=natural_keys)
     for i in range(len(files)):
         filename = files[i]
         if filename.endswith(".mp3"):
